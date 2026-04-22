0d41e787c097b9ec64e6dd5b7288c36c77ae1aa8
Narendra Singh <narendrasingh21208@gmail.com>
2026-04-22 17:11:48 +0530

    Book program

---
+++ oops program/oops program/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oops_program
+{
+    internal class Book
+    {
+        String title;
+        String author;
+        static double price;
+        static Book()
+        {
+            price = 9.99;
+        }
+        public Book(String title, String author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+        public void display()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+}
