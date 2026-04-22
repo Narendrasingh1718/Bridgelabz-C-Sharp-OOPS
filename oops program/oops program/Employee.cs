0b76bd061c086981c6f475c0385d94529eed5d03
Narendra Singh <narendrasingh21208@gmail.com>
2026-04-22 16:58:37 +0530

    Employee program

---
+++ oops program/oops program/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oops_program
+{
+    internal class Employee
+    {
+        String name;
+        int id;
+        double salary;
+        public Employee(String name, int id, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.salary = salary;
+        }
+        public void display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Salary: " + salary);
+        }
+
+    }
+}
